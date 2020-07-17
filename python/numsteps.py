b08f41a328961baa2d36d69d08ccece9a12a0e2c
kieran-github <kierandowds@hotmail.com>
2020-07-17 19:32:39 -0400

    https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/

---
+++ python/numsteps.py
@@ -0,0 +1,15 @@
+"""
+https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+"""
+def numberOfSteps(num):
+    steps = 0
+    while num != 0:
+        if num % 2 == 0:
+            num //= 2
+        else:
+            num -= 1
+        steps += 1
+    return steps
+
+
+print(numberOfSteps(123))
